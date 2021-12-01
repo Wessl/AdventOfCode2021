0394f50af8d0a0736354f7afacc47b54755617bb
Love Wessman <lwessman@kth.se>
2021-12-01 19:35:25 +0100

    ok

---
+++ solutions/solutions/One.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace solutions
+{
+    class One
+    {
+        static void Main()
+        {
+            // Three measurement sliding window
+            string line;
+            List<int> numbers = new List<int>();
+            while ((line = Console.ReadLine()) != null && line != "")
+            {
+                numbers.Add(int.Parse(line));
+            }
+
+            int amountIncreases = -1;
+            int prevSum = 0;
+            for (int i = 1; i < numbers.Count - 1; i++)
+            {
+                int currentSum = numbers[i - 1] + numbers[i] + numbers[i + 1];
+                if (currentSum > prevSum)
+                {
+                    amountIncreases++;
+                }
+                prevSum = currentSum;
+            }
+
+            Console.WriteLine(amountIncreases);
+        }
+    }
+}
